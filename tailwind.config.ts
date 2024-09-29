a21f3ff60b3904c268afa9df811a423e06a483f0
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2024-09-28 23:45:50 -0400

    feat: Install daisyUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,44 @@
 import type { Config } from "tailwindcss"
+import daisyui from "daisyui"
 
 const config: Config = {
-  content: [
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  		},
+  	}
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+],
+  daisyui: {
+    themes: [
+      {
+        "ascension": {
+          "primary": "#e6eed8",
+          "secondary": "#ccd48f",
+          "accent": "#f2de79",
+          "neutral": "#eeeeee",
+          "base-100": "#191303",
+        }
+      },
+      {
+        "escaper": {
+          "primary": "#040402",
+          "secondary": "#03265d",
+          "accent": "#501c83",
+          "neutral": "#eeeeee",
+          "base-100": "#c9ddff",
+        }
+      }
+    ]
+  }
 }
 export default config
