c7ea961a4dac6993bb5fea311533f3acdebb9255
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2024-10-02 22:28:48 -0400

    chore: Replace daisyui with shadcdn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss"
-import daisyui from "daisyui"
 
 const config: Config = {
+    darkMode: ["class"],
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,33 +12,77 @@ const config: Config = {
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
   		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
   	}
   },
   plugins: [
-    daisyui,
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
+	require("tailwindcss-animate")
 ],
-  daisyui: {
-    themes: [
-      {
-        "ascension": {
-          "primary": "#e6eed8",
-          "secondary": "#ccd48f",
-          "accent": "#f2de79",
-          "neutral": "#eeeeee",
-          "base-100": "#191303",
-        }
-      },
-      {
-        "escaper": {
-          "primary": "#040402",
-          "secondary": "#03265d",
-          "accent": "#501c83",
-          "neutral": "#eeeeee",
-          "base-100": "#c9ddff",
-        }
-      }
-    ]
-  }
+//   daisyui: {
+//     themes: [
+// 		{
+// 		  "ascension": {
+// 			"primary": "#040402",
+// 			"secondary": "#03265d",
+// 			"accent": "#501c83",
+// 			"neutral": "#eeeeee",
+// 			"base-100": "#c9ddff",
+// 		  }
+// 		},
+// 		{
+// 			"escaper": {
+// 			"primary": "#e6eed8",
+// 			"secondary": "#ccd48f",
+// 			"accent": "#f2de79",
+// 			"neutral": "#eeeeee",
+// 			"base-100": "#191303",
+// 			}
+// 		}
+//     ]
+//   }
 }
 export default config
