39569e8340591164fe66811adb688f7eaceff501
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2024-10-20 17:59:02 -0400

    feat: Create Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <footer className="footer footer-center text-xs text-primary py-3">
-            <aside>
+        <footer className="w-full py-3 text-xs text-primary">
+            <aside className="flex flex-col items-center">
                 <p>Built and designed by Jordon Hong.</p>
-                <p>All rights reserved. © Jordon Hong {new Date().getFullYear()}.</p>
+                <p>All rights reserved. Jordon Hong © {new Date().getFullYear()}.</p>
             </aside>
         </footer>
     )
